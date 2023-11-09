7e152e32e6e4fbd328b63db92f9678778c9a8cce
dinabarrish <dinabarrish@utexas.edu>
2023-11-08 22:26:49 -0600

    saving all the new stuff

--- tasks/format.js
+++ tasks/format.js
@@ -1,5 +1,5 @@
 import gulp from 'gulp';
-import prettier from 'gulp-plugin-prettier';
+
 
 function format() {
   return gulp
@@ -10,12 +10,7 @@ function format() {
       './src/js/*.js',
       './src/_data/*.json'
     ])
-    .pipe(
-      prettier.format({
-        config: '.prettierrc.json'
-      })
-    )
-    .pipe(gulp.dest((file) => file.base));
+  
 }
 
 gulp.task('format', format);
